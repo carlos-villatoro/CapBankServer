19f4a796846a58b486e6979f2a3dcff5a42b2f1e
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-07-20 02:05:11 -0500

    integrates index.html as index.jsp

--- src/main/java/com/capbank/capbankserver/config/AppConfig.java
+++ src/main/java/com/capbank/capbankserver/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.capbank.capbankserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -10,6 +11,12 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @ComponentScan(basePackages = {"com.capbank.capbankserver"})
 public class AppConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("css/**", "images/**", "js/**")
+                .addResourceLocations("classpath:/static/css/", "classpath:/static/images/", "classpath:/static/js/");
+    }
 
     @Bean
     public InternalResourceViewResolver viewResolver(){
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,4 +1,34 @@
-Index page
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<br>
-${ PageTitle }
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="css/fontawesome/css/all.css">
+    <link rel="stylesheet" href="css/default.css">
+
+    <title>CapBank</title>
+</head>
+<body class="d-flex align-items-center justify-content-end">
+
+<!-- card for the intro text -->
+<div class="card bg-transparent border-0" id="intro-text-card">
+
+    <div class="card-body">
+        <h1>CapBank</h1>
+        <h5 class="card-title">Your money, at your touch</h5>
+    </div>
+
+    <!-- buttons -->
+    <div class="button-wrapper d-flex aligh-items-center">
+        <a href="/register.html" class="btn btn-md register" role="button">Register</a>
+        <a href="/login.html" class="btn btn-md login" role="button">Login</a>
+    </div>
+
+</div>
+
+
+</body>
+</html>

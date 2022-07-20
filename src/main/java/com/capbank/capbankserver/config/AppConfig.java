f704e5d4d7668f179411d745f9b3e4c829d00e6d
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-07-20 04:16:03 -0500

    configs token interceptor

--- src/main/java/com/capbank/capbankserver/config/AppConfig.java
+++ src/main/java/com/capbank/capbankserver/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.capbank.capbankserver.config;
 
+import com.capbank.capbankserver.interceptors.AppInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -26,5 +28,10 @@ public class AppConfig extends WebMvcConfigurationSupport {
         jspViewResolver.setViewClass(JstlView.class);
 
         return jspViewResolver;
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AppInterceptor()).addPathPatterns("/app/*");
     }
 }

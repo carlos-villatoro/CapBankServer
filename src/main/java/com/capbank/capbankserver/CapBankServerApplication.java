bd147974f43beb86113d4f77c90bb8ce8d03d9f4
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-07-20 01:10:34 -0500

    init commit

---
+++ src/main/java/com/capbank/capbankserver/CapBankServerApplication.java
@@ -0,0 +1,13 @@
+package com.capbank.capbankserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapBankServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapBankServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capbank/capbankserver/CapBankServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capbank.capbankserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapBankServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
